2c97ef9eb2e0839c3626375aea30aff7eff874a5
Marius Rak <rak.marius@gmail.com>
2020-07-13 23:20:53 +0200

    Attempt on another approach to path selection

--- MongooseBridge.js
+++ MongooseBridge.js
@@ -1,59 +1,6 @@
-import mongoose from "mongoose/browser";
+import mongoose from "mongoose";
 // import invariant from 'invariant';
 import { Bridge, joinName } from "uniforms";
-
-const findType = props => {
-        if (props.type) {
-                return props.type;
-        }
-        if (Array.isArray(props)) {
-                return Array;
-        }
-        if (typeof props === "object") {
-                return Object;
-        }
-        if (props.type === mongoose.Schema.ObjectId) {
-                return String;
-        }
-        return props;
-};
-
-const normalizeProps = props => {
-        const type = findType(props);
-        const item = type === Array && normalizeProps(props[0]);
-        const subfields = type === Object && normalizePaths(props);
-        return {
-                type,
-                ...(props.default ? { ["default"]: props.default } : {}),
-                ...(subfields ? { subfields } : {}),
-                ...(item ? { item } : {}),
-        };
-};
-
-const normalizePaths = paths => {
-        if (typeof paths === "object") {
-                return Object.keys(paths).reduce(
-                        (normalizedPaths, path) => ({
-                                ...normalizedPaths,
-                                [path]: normalizeProps(paths[path]),
-                        }),
-                        {}
-                );
-        }
-        return normalizeProps(paths);
-};
-
-const traverseSubfields = (tree, name) => {
-        const split = name.match(/([^\.]+)\.?(.*)/);
-        const node = split[1] === "$" ? tree.item : tree[split[1]];
-        if (split[2]) {
-                if (split[2][1]) {
-                        return traverseSubfields(node.subfields || node, split[2]);
-                }
-                return node.item;
-        }
-        return node;
-};
 
 export default class MongooseBridge extends Bridge {
         schema;
@@ -63,38 +10,112 @@ export default class MongooseBridge extends Bridge {
 
                 this.schema = schema;
                 window.schema = schema;
-                this.paths = normalizePaths(this.schema.tree);
                 window.paths = this.paths;
-                this.__fields = {}; // fields name cache so traversing is minimized
+                console.log(this.schema, this.paths);
         }
 
         static check(schema) {
                 return schema && schema instanceof mongoose.Schema;
         }
 
+        _buildPaths() {
+                return [
+                        this.schema.paths,
+                        this.schema.subpaths,
+                        Object.keys(this.schema.nested).reduce(
+                                (nested, n) => ({ ...nested, [n]: { options: { type: Object } } }),
+                                {}
+                        ),
+                ].reduce(
+                        (allPaths, source) => ({
+                                ...allPaths,
+                                ...Object.keys(source).reduce((allPaths, path) => {
+                                        const add = { [path]: source[path] };
+                                        if (source[path].$embeddedSchemaType) {
+                                                add[source[path].$embeddedSchemaType.path] =
+                                                        source[path].$embeddedSchemaType;
+                                        }
+                                        return { ...allPaths, ...add };
+                                }, allPaths),
+                        }),
+                        {}
+                );
+        }
+
+        get paths() {
+                if (!this.__paths) {
+                        this.__paths = this._buildPaths();
+                }
+                return this.__paths;
+        }
+
         getSubfields(name) {
                 if (!name) {
-                        return Object.keys(this.paths);
+                        return Object.keys(this.paths)
+                                .map(f => f.split(".")[0])
+                                .filter((v, i, a) => a.indexOf(v) === i);
                 }
-                const field = this.getField(name);
-                return field.subfields ? Object.keys(field.subfields) : [];
+
+                // console.log("getSubfields(%s)", name, name.replace(/\.[\$\d]+/, ""), name.match(/\.[\$\d]+/));
+                // if (name.match(/\.[\$\d]+$/)) {
+                //         return Object.keys(this.schema.tree[name.replace(/\.[\$\d]+/, "")][0]);
+                // }
+
+                return [];
+        }
+
+        _getPath(name) {
+                return this.paths[name.replace(/\.\d+/, ".$")];
         }
 
         getField(name) {
-                name = name.replace(/\.\d+/g, ".$");
-                if (!this.__fields[name]) {
-                        this.__fields[name] = traverseSubfields(this.paths, name);
+                console.log("getField(%s)", name, this._getPath(name));
+                return this._getPath(name).options;
+
+                /*
+                if (name.match(/\.[\$\d]+/)) {
+                        return { type: Object };
+                        const field = this.schema.tree[name.replace(/\.[\$\d]+/, "")];
+                        return field[0];
                 }
-                return this.__fields[name];
+
+                const field = this.schema.tree[name];
+
+                // console.log('getField(%s)', name, field);
+
+                if (Array.isArray(field)) {
+                        return { type: Array };
+                }
+                if (!field.type && typeof field === "object") {
+                        return { type: Object };
+                }
+                if (typeof field === "function") {
+                        return { type: field };
+                }
+                if (field.auto === true && field.type === "ObjectId") {
+                        return { ...field, type: String };
+                }
+
+                return field;*/
         }
 
         getType(name) {
-                return this.getField(name).type;
+                const path = this._getPath(name);
+                if (path.$isMongooseArray) {
+                        return Array;
+                }
+                if (path.schema) {
+                        return Object;
+                }
+                return path.options.type;
         }
 
         getProps(name, props = {}) {
                 // Type should be omitted.
-                let { optional, type, uniforms, ...field } = this.getField(name);
+                // eslint-disable-next-line no-unused-vars, prefer-const
+                let { optional, uniforms, ...field } = this.getField(name);
+                const type = this.getType(name);
+                console.log("getProps(%s), type: ", name, type);
 
                 field = { ...field, required: !optional };
 
@@ -144,14 +165,23 @@ export default class MongooseBridge extends Bridge {
                         }
                 }
 
-                field.label = field.label || name.replace(/\.\d+/g, ".$");
+                field.label = field.label || name;
+                field.defaultValue = field.default || "";
+                delete field.default;
                 field.fieldType = typeof type === "function" ? type : type.constructor;
+                // if (Array.isArray(type)) {
+                //         field.fieldType = Array;
+                // }
+                // if (typeof type === "object") {
+                //         field.fieldType = Object;
+                // }
 
                 return field;
         }
 
         getInitialValue(name, props = {}) {
                 const field = this.getProps(name);
+                console.log(name, field);
 
                 if (field.fieldType === Array) {
                         const item = this.getInitialValue(joinName(name, "0"));
@@ -163,17 +193,11 @@ export default class MongooseBridge extends Bridge {
                         return {};
                 }
 
-                return typeof field.default === "function" ? field.default() : field.default;
+                return typeof field.defaultValue === "function" ? field.defaultValue() : field.defaultValue;
         }
 
         getValidator(options = { clean: true, mutate: true }) {
-                return data => {
-                        const err = new mongoose.Document(data, this.schema).validateSync();
-                        if (err) {
-                                console.log(err);
-                                throw err;
-                        }
-                };
+                model => new mongoose.model("", this.schema)(model);
 
                 /*
                 const validator = this.schema.validate(options);
@@ -187,36 +211,29 @@ export default class MongooseBridge extends Bridge {
         }
 
         getError(name, error) {
-                const err = error && error.errors && error.errors[name.replace(/\.\d+/g, ".$")];
-                return (err && err.properties && err.properties.message) || err;
-                /*
-                console.log("getError", name);
                 return (
                         (error &&
                                 error.details &&
                                 error.details.find &&
                                 error.details.find(error => error.name === name)) ||
                         null
-                );*/
+                );
         }
 
         getErrorMessage(name, error) {
-                return this.getError(name, error) || "";
                 const scopedError = this.getError(name, error);
                 return !scopedError ? "" : this.schema.messageForError(scopedError);
         }
 
         getErrorMessages(error) {
                 if (error) {
-                        return Object.values(error.errors).map(e => e.properties.message);
-                        /*
                         if (Array.isArray(error.details)) {
                                 return error.details.map(error => this.schema.messageForError(error));
                         }
 
                         if (error.message) {
                                 return [error.message];
-                        }*/
+                        }
                 }
 
                 if (error !== undefined) {
